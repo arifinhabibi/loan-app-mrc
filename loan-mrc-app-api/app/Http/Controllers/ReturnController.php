ed15f73d076485f840bd6de79580be46c8c583da
muhammad arifin habibi <arifinhabibi76@gmail.com>
2022-11-25 10:16:42 +0700

    save project

--- loan-mrc-app-api/app/Http/Controllers/ReturnController.php
+++ loan-mrc-app-api/app/Http/Controllers/ReturnController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Loan;
 use App\Models\User;
+use App\Models\Goods;
+use App\Models\GoodsLoan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,13 +13,25 @@ class ReturnController extends Controller
 {
     // return goods
     public function returning(Request $request, $id){
-        $loan = Loan::find($id);
+        $loan = Loan::with(['goodsLoans'])->find($id);
 
         if (!$loan) {
             # code...
             return response()->json([
                 'message' => 'data peminjaman tidak ditemukan'
             ], 401);
+        }
+
+        $goodsLoans = GoodsLoan::where('loan_id', $loan->id)->get();
+
+        foreach ($goodsLoans as $goodsLoan) {
+            # code...
+            $goods = Goods::find($goodsLoan->goods_id);
+
+            $goods->update([
+                'stock' => $goods->stock + $goodsLoan->quantity
+            ]);
+
         }
 
         $user = User::where('tokens', $request->token)->first();
